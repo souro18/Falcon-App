a6c4d2a97cfc44be8ced6e3861a54612260e758e
souro18 <sourajit.paul61@gmail.com>
2020-05-23 11:18:31 +0530

    Updates

---
+++ precache-manifest.1d3d1b3ebbd7cdf39a9df48b2d495cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d48f10454b03b3308955639591d4cc",
+    "url": "/Falcon-App/index.html"
+  },
+  {
+    "revision": "a96daaedec108b0759ed",
+    "url": "/Falcon-App/static/css/main.236e34a9.chunk.css"
+  },
+  {
+    "revision": "fe3df024204e36187aa8",
+    "url": "/Falcon-App/static/js/2.635403ee.chunk.js"
+  },
+  {
+    "revision": "a96daaedec108b0759ed",
+    "url": "/Falcon-App/static/js/main.9df4ebae.chunk.js"
+  },
+  {
+    "revision": "95153d7341d9cc982417",
+    "url": "/Falcon-App/static/js/runtime-main.0039e258.js"
+  },
+  {
+    "revision": "68cd4fde6a20a419ad279aa9f74e7fa5",
+    "url": "/Falcon-App/static/media/Everspace.68cd4fde.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Falcon-App/precache-manifest.1d3d1b3ebbd7cdf39a9df48b2d495cd0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Falcon-App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
